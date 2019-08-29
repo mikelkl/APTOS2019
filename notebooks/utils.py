f5d175d83e44667a35fdb82c2d454206e554bbc1
mikelkl <mikelkl@foxmail.com>
2019-08-29 18:10:49 +0800

    init

---
+++ notebooks/utils.py
@@ -0,0 +1,46 @@
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+
+from datetime import datetime, timedelta, timezone
+
+def get_BJ_time():
+    # 拿到UTC时间，并强制设置时区为UTC+0:00
+    utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
+    # astimezone()将转换时区为北京时间
+    bj_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))
+    current_time = bj_dt.strftime('%m%d_%H-%M-%S')
+
+    return current_time
+
+# 绘制混淆矩阵
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    Input
+    - cm : 计算出的混淆矩阵的值
+    - classes : 混淆矩阵中每一行每一列对应的列
+    - normalize : True:显示百分比, False:显示个数
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
